8cbdc30c96799af0025131a89c362955a6666f6e
ayah-j-nada <ayah.j.nada8@gmail.com>
2023-07-29 15:26:01 +0300

    First Commit

---
+++ T1/Task1.py
@@ -0,0 +1,6 @@
+Name = input("Please enter your name: ")
+Age = input("Please enter your age: ")
+DOB = input("Please enter your date of birth: ")
+print("your name is: " + Name)
+print("your age is: " + Age)
+print("your date of birth is: " + DOB)
