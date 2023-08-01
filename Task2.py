d16b0f16f6cdc7674c31fbadfe41bd1f0566820e
ayah-j-nada <ayah.j.nada8@gmail.com>
2023-08-01 10:19:21 +0300

    Second Commit

--- Task2.py
+++ Task2.py
@@ -2,8 +2,8 @@ name = input("Enter your full name: ")
 mobile = input("Enter yor mobile number (05x-xxxx-xxx):  ")
 year_of_birth = int(input("Enter your year of birth: "))
 user_age = 2023 - year_of_birth
-print("Name: " + name)
-print("Age: " + str(user_age))
+print(f"Name: {name}")
+print(f"Age: {user_age}")
 mobile = mobile.split("-")
 print(f"Mobile: {mobile}")
 
